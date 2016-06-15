8f09a5f139ae3e6f2fa3af137b85c80d8f506b49
lytwaytLaz <laszlo.hago@gmail.com>
2016-06-15 22:28:33 +0200

    Add now-time alteration

--- src/lesson_7_1/Touch.java
+++ src/lesson_7_1/Touch.java
@@ -5,9 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
+import java.time.DateTimeException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 /**
  * @author László Hágó
@@ -15,30 +15,33 @@ import java.time.format.DateTimeParseException;
  * @since 2016-06-15
  */
 public class Touch {
-    public static void main(String... args) {
-        if (args.length == 0) {
-            if (args[0].equals("-d") && args.length == 5) {
-                System.err.println("Usage: java Touch [-d yyyy.MM.dd HH:mm:ss z] pathname");
-                System.exit(1);
-            }
+    public static void main(String[] args) throws DateTimeException, IOException
+    {
+        if (args.length != 1 && args.length != 5) {
+            System.err.println("Usage: java Touch [-d yyyy.MM.dd HH.mm.ss z] pathname");
+            System.exit(1);
         }
 
-        String timeDate = args[1] + " " + args[2] + " " + args[3];
-        Path p1 = Paths.get(args[4]);
+        if (!args[0].equals("-d")) {
+            Path p1 = Paths.get(args[0]);
+            ZonedDateTime now = ZonedDateTime.now();
+            Files.setLastModifiedTime(p1, FileTime.from(now.toInstant()));
 
-        try {
-            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss z");
+            System.out.println("Time: " + now.toString());
+        }
+
+        else
+        {
+            String timeDate = args[1] + " " + args[2] + " " + args[3];
+            Path p1 = Paths.get(args[4]);
+
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy.MM.dd HH.mm.ss z");
             ZonedDateTime zonTid = ZonedDateTime.parse(timeDate, format);
             Files.setLastModifiedTime(p1, FileTime.from(zonTid.toInstant()));
+
             System.out.println("Time: " + zonTid.toString());
-        } catch (DateTimeParseException|IOException e) {
-            e.printStackTrace();
         }
-//        FileTime fileTime = FileTime.fromMillis(System.currentTimeMillis());
 
     }
-//            FileTime now = FileTime.fromMillis(System.currentTimeMillis());
-
-
 }
 
---
+++ src/lesson_7_5/BinarySearch.java
@@ -0,0 +1,10 @@
+package lesson_7_5;
+
+/**
+ * @author László Hágó
+ * @version 1.0
+ * @since 2016-06-15
+ */
+public class BinarySearch
+{
+}
