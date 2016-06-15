f772e8be9af74a296781da14cb4a47005f1ecf7b
lytwaytLaz <laszlo.hago@gmail.com>
2016-06-15 22:48:04 +0200

    Start assignment 7_5

--- src/lesson_7_5/BinarySearch.java
+++ src/lesson_7_5/BinarySearch.java
@@ -4,7 +4,19 @@ package lesson_7_5;
  * @author László Hágó
  * @version 1.0
  * @since 2016-06-15
+ * <nyckel>:<element1>,<element 2>,<element n>
+ *
+ * copy
+ *
+ *
+ *
  */
+
+
 public class BinarySearch
 {
+    public static void main(String[] args)
+    {
+
+    }
 }
